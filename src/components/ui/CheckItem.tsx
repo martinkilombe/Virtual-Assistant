4414b5229d7452d441ad26d6eedb79fc7beb4381
Martin Muti <martinkilombe@gmail.com>
2026-06-24 23:19:03 +0300

    Services page

--- src/components/ui/CheckItem.tsx
+++ src/components/ui/CheckItem.tsx
@@ -1,11 +1,7 @@
-import { IconCheck } from '@/components/icons'
-
 export default function CheckItem({ children }: { children: React.ReactNode }) {
   return (
     <li className="iva-check-item">
-      <span className="iva-check-item__icon">
-        <IconCheck size={14} />
-      </span>
+      <span className="iva-check-item__icon" aria-hidden="true" />
       {children}
     </li>
   )
